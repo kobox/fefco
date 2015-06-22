7300765056ee04a9a0ee86e9b3fe01e02a4ea1d0
Krzysztof Olszewski <k.olszewski@o2.pl>
2015-06-22 15:19:52 +0200

    initial

---
+++ fefcocatalog/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+# Create your models here.
+
+class FefcoCategory(models.Model):
+
+    slug = models.SlugField(max_length=50)
+    title = models.CharField('Tytuł', default='', max_length=254, blank=False, null=False)
+    description = models.TextField('Opis', blank=True, null=True)
+    picture = models.ImageField('Kategoria FEFCO', default='fefco_pics/default.png', upload_to='fefco_pics/',
+                                blank=False, null=False)
+    fefcocode = models.CharField(max_length=5)
+    parent_category = models.ForeignKey('self', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
